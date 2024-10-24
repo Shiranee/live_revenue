9cd907b722805f303ca4f33d349beb6bbe37f663
Shiranai <viniciuspazo@yahoo.com.br>
2024-10-23 23:33:26 -0300

    started dashboard

--- public/dashboards/revenue_ecomm/index.php
+++ public/dashboards/revenue_ecomm/index.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Conciliator Dashboard</title>
+	<title>LIVE! Receita E-commerce</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
 	<script src="https://cdn.jsdelivr.net/npm/echarts@5.5.0/dist/echarts.min.js"></script>
@@ -25,10 +25,10 @@
 
 										<div class="row justify-content-start">
 											<div class="col">
-												<h6 class="text-body flex-1 mb-0">Completed</h6>
+												<h6 class="text-body flex-0 mb-0">Pagamentos Confirmados: 19.000</h6>
 											</div>
 											<div class="col">
-												One of two columns
+												<h6 class="text-body flex-0 mb-0">Pagamentos Pendentes: 19.000</h6>
 											</div>
 										</div>
                     
@@ -55,20 +55,6 @@
                 <div class="card-body">
                     <h5 class="card-title fw-bold">Receita Acumulada</h5>
 
-										<div class="container text-center">
-											<div class="row">
-												<div class="col">
-													Clientes
-												</div>
-												<div class="col">
-													Pedidos
-												</div>
-												<div class="col">
-													Peças
-												</div>
-											</div>
-										</div>
-
                 </div>
             </div>
         </div>
@@ -78,19 +64,6 @@
                 <div class="card-body">
                     <h5 class="card-title fw-bold">Receita Faturada</h5>
 
-										<div class="container text-center">
-											<div class="row">
-												<div class="col">
-													Clientes
-												</div>
-												<div class="col">
-													Pedidos
-												</div>
-												<div class="col">
-													Peças
-												</div>
-											</div>
-										</div>
 
                 </div>
             </div>
